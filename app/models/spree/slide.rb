7d3a65bfae32a509eaf9c6a306c6be2a2c300ef9
Jason Knebel <jasonkneb@gmail.com>
2015-11-19 20:08:58 -0500

    using S3/Cloudfront to deliver slider images

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,5 +1,15 @@
 class Spree::Slide < ActiveRecord::Base
-  has_attached_file :image
+  has_attached_file :image,
+    storage:             :s3,
+    s3_credentials: {
+      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    },
+    bucket:              ENV['S3_BUCKET_NAME'],
+    url:                 ':s3_alias_url',
+    s3_host_alias:       ENV['CLOUDFRONT_DOMAIN'],
+    path:                '/slider/:id/:style/:basename.:extension'
+
   scope :published, where(:published => true)
   attr_accessible :name, :body, :link_url, :published, :image
 end
